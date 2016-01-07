3dd4e3fbb804c5ca5ad1d69cc6e66bc8262eac33
Pat Killean <pat.killean@gmail.com>
2016-01-07 08:43:09 -0500

    setup reader ns

--- src/cljs_node_io/reader.cljs
+++ src/cljs_node_io/reader.cljs
@@ -1,1 +1,12 @@
-(ns  cljs-node-io.reader)
+(ns  cljs-node-io.reader
+  ; (:require-macros [cljs-node-io.macros :refer [with-open]])
+  (:require [cljs.nodejs :as nodejs :refer [require]]
+            ; [cljs-node-io.streams :refer [FileInputStream]]
+            [cljs-node-io.util :refer [append?
+                                       IGetType get-type
+                                       Coercions as-url as-file
+                                       IOFactory make-reader make-writer make-input-stream make-output-stream]]))
+
+
+
+(defn reader [input-stream opts ] input-stream)
