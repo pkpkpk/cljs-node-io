e33e89a1f37a600af214dae176971aac1ee603ce
Pat Killean <pat.killean@gmail.com>
2016-01-07 21:20:46 -0500

    src/cljs_node_io/reader.cljs
    switch to protocols dependency

--- src/cljs_node_io/reader.cljs
+++ src/cljs_node_io/reader.cljs
@@ -2,10 +2,10 @@
   ; (:require-macros [cljs-node-io.macros :refer [with-open]])
   (:require [cljs.nodejs :as nodejs :refer [require]]
             ; [cljs-node-io.streams :refer [FileInputStream]]
-            [cljs-node-io.util :refer [append?
-                                       IGetType get-type
-                                       Coercions as-url as-file
-                                       IOFactory make-reader make-writer make-input-stream make-output-stream]]))
+            [cljs-node-io.protocols
+             :refer [IGetType get-type
+                     Coercions as-url as-file
+                     IOFactory make-reader make-writer make-input-stream make-output-stream]]))
 
 
 
