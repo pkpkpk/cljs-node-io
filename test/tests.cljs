295d520212cf346b92767c3fa0daae6da91335a4
Pat Killean <pat.killean@gmail.com>
2016-01-07 21:13:48 -0500

    change to protocols ns

--- test/tests.cljs
+++ test/tests.cljs
@@ -1,7 +1,7 @@
 (ns ^:figwheel-always cljs-node-io.tests
   (:require [cljs.test :refer-macros [deftest is testing run-tests are]]
             [cljs-node-io.file :refer [File]]
-            [cljs-node-io.util :refer [Coercions as-file as-url ]]
+            [cljs-node-io.protocols :refer [Coercions as-file as-url ]]
             [cljs-node-io.core :refer [file as-relative-path]]) ;file File
   (:import goog.Uri))
 
