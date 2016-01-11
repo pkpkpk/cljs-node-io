bf522aee4166683caf7a3eee073c37bd8cbf2cdf
Pat Killean <pat.killean@gmail.com>
2016-01-11 16:17:06 -0500

    src/cljs_node_io/streams.cljs
    basic file streams

--- src/cljs_node_io/streams.cljs
+++ src/cljs_node_io/streams.cljs
@@ -3,49 +3,109 @@
             [cljs-node-io.protocols
               :refer [IGetType get-type
                       Coercions as-url as-file
-                      IOFactory make-reader make-writer make-input-stream make-output-stream]])
-  )
+                      IOFactory make-reader make-writer make-input-stream make-output-stream]]))
 
 (def fs (require "fs"))
 
+(defn ^Boolean isFd? "is File-descriptor?"
+  [path]
+  (= path (unsigned-bit-shift-right path 0)))
 
-(def default-options
-  {:flags "r"
-   :encoding "utf8"
-  ;  :fd nil
-   :mode "0o666"
+
+(defn file-stream-dispatch [f opts]
+  (if (or (isFd? f) (isFd? (:fd opts)))
+    :file-descriptor
+    (get-type f)))
+
+
+
+
+; Note that fd should be blocking; non-blocking fds should be passed to net.Socket.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def default-input-options
+  {
+  ;  :flags "r" ;-> under the hood default
+  ;  :encoding "utf8" ;-> under the hood default string literal
+  ;  :mode "0o666" ;-> under the hood default
+   :fd nil ;if specified, will ignore path argument, therefore no 'open' event
    :autoClose true})
 
-(defmulti FileInputStream* get-type) ;if gonna check for isFd need custom dispatcher here
-
-(defmethod FileInputStream* :file-descriptor [& args] nil) ;maybe can ignore those because node checks for this internally
-
-(defmethod FileInputStream* :string [pathstring] ;should never reach this, should be coerced to file or URi
-  (let [filestreamobj (.createReadStream fs pathstring)] ;should check path validity, URI too
+(defn attach-input-impls! [filestreamobj]
+  (let [filedesc      (atom nil)
+        _             (.on filestreamobj "open" (fn [fd] (reset! filedesc fd )))]
     (specify! filestreamobj
       IGetType
       (get-type [_] :FileInputStream)
       IOFactory
-      (make-reader [this opts] this)
-      )
-    filestreamobj))
+      (make-reader [this _] this)
+      (make-input-stream [this _] this)
+      (make-writer [this _] (throw (js/Error. (str "ILLEGAL ARGUMENT: Cannot open <" (pr-str this) "> as an OutputStream."))))
+      (make-output-stream [this _] (throw (js/Error. (str "ILLEGAL ARGUMENT: Cannot open <" (pr-str this) "> as an OutputStream."))))
+      Object
+      (getFd [_] @filedesc)))
+    filestreamobj)
 
-(defmethod FileInputStream* :file [file]
-  (let [filestreamobj (.createReadStream fs (.getPath file))] ;handle path, URI too
+(defmulti FileInputStream* file-stream-dispatch)
+
+(defmethod FileInputStream* :file-descriptor [fd opts]
+  (let [filestreamobj (.createReadStream fs nil (clj->js opts))]
+    (attach-input-impls! filestreamobj)))
+
+(defmethod FileInputStream* :string [pathstring opts] ;should never reach this via IOFactor, should be coerced to file or Uri
+  (let [filestreamobj (.createReadStream fs pathstring (clj->js opts)) ] ;should check path validity, URI too
+    (attach-input-impls! filestreamobj)))
+
+(defmethod FileInputStream* :file [file opts]
+  (let [filestreamobj (.createReadStream fs (.getPath file) (clj->js opts))] ;handle path, URI too
+    (attach-input-impls! filestreamobj)))
+
+(defn FileInputStream ; file|string|file-descriptor => stream-object => Buffer
+  ([file] (FileInputStream* file default-input-options))
+  ([file opts] (FileInputStream* file (merge default-input-options opts))))
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def default-output-options
+  { :flags "w" ;r+ = append?
+    :defaultEncoding "utf8" ; can be any encoding accepted by Buffer
+    :fd nil ;if specified, will ignore path argument, therefore no 'open' event
+    ; :start -> used to write at some postion past the beginning of the file
+    ; :mode "0o666" ;needs to be parsed
+   })
+
+(defn attach-output-impls! [filestreamobj]
+  (let [filedesc      (atom nil)
+        _             (.on filestreamobj "open" (fn [fd] (reset! filedesc fd )))]
     (specify! filestreamobj
       IGetType
-      (get-type [_] :FileInputStream)
+      (get-type [_] :FileOutputStream)
       IOFactory
-      (make-reader [this opts] this)
-      (make-input-stream [this opts] (println  "tried to make input stream out of existing stream") this)
-      )
-    filestreamobj))
+      (make-reader [this _] (throw (js/Error. (str "ILLEGAL ARGUMENT: Cannot open <" (pr-str this) "> as an InputStream."))))
+      (make-input-stream [this _] (throw (js/Error. (str "ILLEGAL ARGUMENT: Cannot open <" (pr-str this) "> as an InputStream."))))
+      (make-writer [this _] this)
+      (make-output-stream [this _] this)
+      Object
+      (getFd [_] @filedesc)))
+    filestreamobj)
+
+
+(defmulti FileOutputStream* (fn [f opts] (get-type f)))
+
+(defmethod FileOutputStream* :string [pathstring opts] ;should never reach this via IOFactor, should be coerced to file or Uri
+  (let [filestreamobj (.createWriteStream fs pathstring (clj->js opts)) ] ;should check path validity, URI too
+    (attach-output-impls! filestreamobj)))
+
+(defn FileOutputStream
+  ([file] (FileOutputStream* file default-input-options))
+  ([file opts] (FileOutputStream* file (merge default-input-options opts))))
 
 
 
-(defn FileInputStream
-  ([file] (FileInputStream* file))
-  ([file opts] (FileInputStream* file)))
-
-
-(deftype FileOutputStream [x y])
+    ;
+    ; writeStream.bytesWritten#
+    ; The number of bytes written so far. Does not include data that is still queued for writing.
