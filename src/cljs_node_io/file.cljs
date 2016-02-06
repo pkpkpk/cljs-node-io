18f7c21ee38c77398e85061ab51fb4a936ae97d6
Pat Killean <pat.killean@gmail.com>
2016-02-06 16:34:57 -0500

    -a
    add file methods and most tests

--- src/cljs_node_io/file.cljs
+++ src/cljs_node_io/file.cljs
@@ -16,20 +16,29 @@
   "true iff file denoted by this abstract pathname exists and is a directory"
   ^boolean
   [^String pathstring]
+  (assert (string? pathstring) "directory? takes a string, perhaps you passed a file instead")
   (let [stats (try (.statSync fs pathstring) (catch js/Error e false))]
     (if stats
       (.isDirectory stats)
       false)))
 
-(defn get-dirs
+(defn file?
+  "true iff file denoted by this abstract pathname exists and is a file"
+  ^boolean
+  [^String pathstring]
+  (assert (string? pathstring) "file? takes a string, perhaps you passed a file instead")
+  (let [stats (try (.statSync fs pathstring) (catch js/Error e false))]
+    (if stats
+      (.isFile stats)
+      false)))
+
+(defn get-non-dirs
   "returns sequence of strings representing non-existing directory components
    of the passed pathstring, root first, in order "
   [^String pathstring]
   (reverse (take-while #(not (directory? %)) (iterate #(.dirname path %) pathstring))))
 
-(defn has-ext? ^boolean [^String pathstring] (not= "" (.extname path pathstring)))
-
-(defn ^Boolean append? [opts] (boolean (:append opts)))
+(defn ^boolean append? [opts] (boolean (:append opts)))
 
 (defn filepath-dispatch
   ([x] (type x)) ; string || Uri.
@@ -38,8 +47,8 @@
 (defmulti  filepath "signature->File" filepath-dispatch)
 (defmethod filepath Uri [u] (.getPath u))
 (defmethod filepath js/String  [pathstring] pathstring)
-(defmethod filepath [js/String js/String] [parent-str child-str] (str parent-str "/" child-str))
-(defmethod filepath [:File js/String] [parent-file child-str] (str (.getPath parent-file) "/" child-str))
+(defmethod filepath [js/String js/String] [parent-str child-str] (str parent-str (.-sep path) child-str))
+(defmethod filepath [:File js/String] [parent-file child-str] (str (.getPath parent-file) (.-sep path) child-str))
 (defmethod filepath :default [x] (throw (js/Error.
                                          (str "Unrecognized path configuration passed to File constructor."
                                               "\nYou passed " (pr-str x)
@@ -51,30 +60,36 @@
 (defn file-stream-writer [filestream opts]
   (make-writer filestream opts)) ;just defering to file stream object for now
 
-(defn file-reader [file opts]
-  ;hide inside a .read method? channel options?
+(defn file-reader
+  "Builds an appropriate read method given opts and attaches it to the reified file.
+   Returns the passed file.
+    TODO if :stream? true, returns a chan which receives FileInputStream asynchronously?
+    - opts: encoding :async :stream?
+    - if :async? true, file.read() returns a chan which receives err|str on successful read "
+  [file opts]
   (if (:stream? opts)
     (file-stream-reader (make-input-stream file opts) opts)
-      (if (:async? opts)
-        (specify! file Object
-          (read [this opts]
-            (let [c (chan) ]
-              (.readFile fs (.getPath this) (or (:encoding opts) "utf8") ;if no encoding, returns buffer
-                (fn [err data] (put! c (or err data))))
-              c)))
-        (specify! file Object
-          (read [this opts]
-            (.readFileSync fs (.getPath this) (or (:encoding opts) "utf8"))))))) ;if no encoding, returns buffer . catch err?
+    (if (:async? opts)
+      (specify! file Object
+        (read [this opts]
+          (let [c (chan) ]
+            (.readFile fs (.getPath this) (or (:encoding opts) "utf8") ;if no encoding, returns buffer
+              (fn [err data] (put! c (or err data))))
+            c)))
+      (specify! file Object
+        (read [this opts]
+          (.readFileSync fs (.getPath this) (or (:encoding opts) "utf8"))))))) ;if no encoding, returns buffer . catch err?
 
 
 
 
 (defn file-writer
   "Builds an appropriate write method given opts and attaches it to the reified file.
-    - TODO: support flags in opts?
-    - encoding, append?, async?, stream?
+   Returns the passed file.
+    TODO if :stream? true, returns a chan which receives FileOutputStream asynchronously?
+    - opts: encoding, append?, async?, stream?
     - if content is a Buffer instance, opt encoding is ignored
-    - if :async? true, returns a chan which recieves err|true on success"
+    - if :async? true, file.write() returns a chan which receives err|true on successful write."
   [file opts]
   (if (:stream? opts) ;async write option too
     (file-stream-writer (make-output-stream file opts) opts)
@@ -83,74 +98,159 @@
         (write [this content]
           (let [filename (.getPath this)
                 c (chan)
-                cb (fn [err] (put! c (or err true)))] ;mode? more flags?
-            (.writeFile fs filename content  #js{"flag" (if (:append? opts) "a" "w")
+                cb (fn [err] (put! c (or err true)))] ;mode?
+            (.writeFile fs filename content  #js{"flag"     (or (:flags opts) (if (:append opts) "a" "w"))
                                                  "encoding" (or (:encoding opts) "utf8")} cb)
             c)))
       (specify! file Object ;sync
         (write [this content]
-          (let [filename (.getPath this)] ;mode? more flags?
+          (let [filename (.getPath this)] ;mode?
             (.writeFileSync fs filename content
-                            #js{"flag"     (if (:append? opts) "a" "w")
+                            #js{"flag"     (or (:flags opts) (if (:append opts) "a" "w"))
                                 "encoding" (or (:encoding opts) "utf8")})))))))
 
 
 
-(defn File* [pathstring]
-  (reify
-    IEquiv
-    (-equiv [this that]
-      (let [pathntype (juxt #(.-getPath %) type)]
-        (= (pathntype this) (pathntype that))))
-    Coercions
-    (as-file [f] f)
-    (as-url [f] (.to-url f))
-    IOFactory
-    (make-reader [^File this opts] (file-reader this opts))
-    (make-writer [^File this opts] (file-writer this opts))
-    (make-input-stream [^File this opts] (FileInputStream. this opts))
-    (make-output-stream [^File this opts] (FileOutputStream. this (append? opts)) opts);?????????
-    Object
-    (delete [this] (try
-                     (do
-                       (.unlinkSync fs pathstring)
-                       true)
-                     (catch js/Error e false)))
-    (deleteOnExit [this] (.on js/process "exit" #(.delete this)))
-    (exists [this] (.existsSync fs pathstring)) ;deprecated buts stats docs are shit so im using it
-    (toURI [f] (Uri. pathstring))
-    (getAbsolutePath [f] (.resolve path pathstring))
-    (getPath [f] (if (.isAbsolute  f) (.getPath (Uri. pathstring))  pathstring))
-    (isDirectory [_] (directory? pathstring)) ;=> Boolean
-    (isAbsolute [_] (.isAbsolute path pathstring)) ;=>Boolean
-    (getParentFile [_] (as-file (.dirname path pathstring))) ;=> File|nil
-    (mkdirs [this] ; Creates the directory named by this abstract pathname, including any necessary but nonexistent parent directories.
-      (let [p  (.getPath this)
-            dirs (get-dirs p)]
-        (try
-          (do
-            (doseq [d dirs]
-              (if (not (directory? d))
-                (.mkdirSync fs d)))
-            true); true iff the directory was created, along with all necessary parent directories;
-          (catch js/Error e false)))))) ; if false it may have succeeded in creating some of the necessary parent directories
+(defn File* [pathstr]
+  (let [pathstring  (atom pathstr)
+        f           (reify
+                      IEquiv
+                      (-equiv [this that]
+                        (let [pathntype (juxt #(.-getPath %) type)]
+                          (= (pathntype this) (pathntype that))))
+                      Coercions
+                      (as-file [f] f)
+                      (as-url [f] (.to-url f))
+                      IOFactory
+                      (make-reader [^File this opts] (file-reader this opts))
+                      (make-writer [^File this opts] (file-writer this opts))
+                      (make-input-stream [^File this opts] (FileInputStream. this opts))
+                      (make-output-stream [^File this opts] (FileOutputStream. this (append? opts)) opts)
+                      IPrintWithWriter
+                      (-pr-writer [this writer opts] ;#object[java.io.File 0x751b0a12 "foo\\bar.txt"]
+                        (-write writer "#object [cljs-node-io.File")
+                        (-write writer (str "  "  (.getPath this)  " ]")))
+                      Object
+                      ; access tests do nothing when pass, throw when fail
+                      ; https://nodejs.org/api/fs.html#fs_fs_accesssync_path_mode
+                      (canExecute ^boolean [this]
+                        (if-not (= "win32" (.-platform js/process))
+                          (try
+                            (if (nil? (.accessSync fs (.getPath this) (.-X_OK fs))) true)
+                            (catch js/Error e false))
+                          (throw (js/Error "Testing if a file is executable has no effect on Windows "))))
+                      (canRead ^boolean [this]
+                        (try
+                          (if (nil? (.accessSync fs (.getPath this) (.-R_OK fs))) true)
+                          (catch js/Error e false)))
+                      (canWrite ^boolean [this]
+                        (try
+                          (if (nil? (.accessSync fs (.getPath this) (.-W_OK fs))) true)
+                          (catch js/Error e false)))
+                      (createNewFile ^boolean [this]
+                        (file-writer this {:flags "wx" :async? false})
+                        (try
+                          (do
+                            (.write this)
+                            true)
+                          (catch js/Error e false)))
+                      (delete ^boolean [this]
+                        (if (directory? @pathstring)
+                          (try
+                            (do (.rmdirSync fs @pathstring) true)
+                            (catch js/Error e false))
+                          (try
+                             (do (.unlinkSync fs @pathstring) true)
+                             (catch js/Error e false))))
+
+
+                      (deleteOnExit [this]
+                        (.on js/process "exit"  (fn [exit-code] (.delete this))))
+
+
+
+                      (equals ^boolean [this that] (= this that))
+                      (exists ^boolean [this] (.existsSync fs @pathstring)) ;deprecated buts stats docs are shit so im using it
+                      (getAbsoluteFile [this] (as-file (.getAbsolutePath this)))
+                      (getAbsolutePath [_] (.resolve path @pathstring))
+                      (getCanonicalFile [this] (as-file (.getCanonicalPath this)))
+                      (getCanonicalPath [_] (.normalize path @pathstring))
+                      (getName [_] (.-name (.parse path @pathstring)))
+                      (getParent [_] (.dirname path @pathstring))
+                      (getParentFile [this] (as-file (.getParent this))) ;=> File|nil
+                      (getPath ^string [this] (if (.isAbsolute this) (.getPath (Uri. @pathstring))  @pathstring))
+                      (hashCode ^int [_] (hash @pathstring))
+                      (isAbsolute ^boolean [_] (.isAbsolute path @pathstring))
+                      (isDirectory ^boolean [_] (directory? @pathstring))
+                      (isFile ^boolean [_] (file? @pathstring))
+
+                      (lastModified ^int [_]
+                        (let [stats (try (.statSync fs @pathstring) (catch js/Error e false))]
+                          (if stats
+                            (.valueOf (.-mtime stats))
+                            0)))
+                      (length ^int [_]
+                        (let [stats (try (.statSync fs @pathstring) (catch js/Error e false))]
+                          (if stats
+                            (if (.isDirectory stats)
+                              nil
+                              (.-size stats))
+                            0)))
+                      (list [_] ; ^ Vector|nil
+                        (if-not (directory? @pathstring)
+                          nil
+                          (try
+                            (vec (.readdirSync fs @pathstring))
+                            (catch js/Error e nil))))
+                      (list [this filterfn]
+                        (assert (= 2 (.-length filterfn)) "the file filterfn must accept 2 args, the dir File & name string")
+                        (if-let [files (.list this)]
+                          (filterv (partial filterfn @pathstring) files)))
+                      (listFiles [this]
+                        (.list this (fn [d name] (file? (str d (.-sep path)  name)))))
+                      (listFiles [this filterfn]
+                        (assert (= 2 (.-length filterfn)) "the file filterfn must accept 2 args, the dir File & name string")
+                        (if-let [files (.listFiles this)]
+                          (filterv (partial filterfn @pathstring) files)))
+                      (mkdir ^boolean [_]
+                        (try
+                          (do
+                            (.mkdirSync fs @pathstring)
+                            true)
+                          (catch js/Error e false)))
+                      (mkdirs ^boolean [this]
+                        (let [p  (.getPath this)
+                              dirs (get-non-dirs p)]
+                          (try
+                            (do
+                              (doseq [d dirs]
+                                (.mkdirSync fs d))
+                              true)
+                            (catch js/Error e false))))
+                      (renameTo ^boolean [this dest]
+                        (assert (string? dest) "destination must be a string")
+                        (try
+                          (do
+                            (.renameSync fs @pathstring dest)
+                            (reset! pathstring dest)
+                            true)
+                          (catch js/Error e false)))
+                      (toString [_]  @pathstring)
+                      (toURI [f] (Uri. (str "file:" @pathstring))))]
+      (set! (.-constructor f) :File)
+      f))
 
 
 (defn File
-  ([a]
-   (let [f  (File*  (filepath a))]
-     (set! (.-constructor f) :File)
-     f))
-  ([a b]
-   (let [f (File*  (filepath a b))]
-     (set! (.-constructor f) :File)
-     f)))
+  ([a]   (File*  (filepath a)))
+  ([a b] (File*  (filepath a b))))
 
-(defn temp-file
-  ([prefix suffix] (temp-file prefix suffix nil))
-  ([prefix suffix content]
-    (let [tmpd (.tmpdir os)
-          path (str tmpd (.-sep path) prefix "." suffix)
+(defn createTempFile
+  ([prefix] (createTempFile prefix nil nil))
+  ([prefix suffix] (createTempFile prefix suffix nil))
+  ([prefix suffix dir]
+    (let [tmpd (or dir (.tmpdir os))
+          path (str tmpd (.-sep path) prefix (or suffix ".tmp"))
           f    (File. path)
           _    (.deleteOnExit f)]
       f)))
--- test/file.cljs
+++ test/file.cljs
@@ -1,6 +1,59 @@
 (ns cljs-node-io.test.file
   (:require [cljs.test :refer-macros [deftest is testing run-tests are]]
-            [cljs-node-io.file :refer [File temp-file]]
+            [clojure.string :as s :refer [starts-with?]]
+            [cljs-node-io.file :refer [File createTempFile]]
             [cljs-node-io.protocols :refer [Coercions as-file as-url ]]
-            [cljs-node-io.core :refer [spit slurp]]) ;file File
-  (:import goog.Uri))
+            [cljs-node-io.core :refer [spit slurp]]))
+
+
+; test delete & empty v full directories
+
+(deftest test-createNewFile-and-delete
+  (let [f (createTempFile "foo" ".txt")]
+    (is (= false (.exists f)))
+    (is (= 0     (.length f)))
+    (is (= true  (.createNewFile f)))
+    (is (= false (zero? (.length f))))
+    (is (= true  (.exists f)))
+    (is (= nil   (.list f)))
+    (is (= false (.createNewFile f)))
+    (is (= true  (.delete f)))
+    (is (= false (.exists f)))
+    (is (= false (.delete f)))))
+
+
+(deftest test-directory-functions
+  (let [d  (createTempFile "TEST_DIRECTORY" "")
+        d2 (createTempFile "subdir" "" d)
+        d3 (createTempFile "subdir2" "" d2)
+        foo  (createTempFile "foo" nil d)
+        bar  (createTempFile "bar" nil d)
+        deep  (createTempFile "deep" nil d3)]
+    (.delete d)
+    ; create & delete empty directory
+    (is (= false (.exists d)))
+    (is (= true  (.mkdir d)))
+    (is (= true  (.exists d)))
+    (is (= true  (.isDirectory d)))
+    (is (= true  (empty? (.list d))))
+    (is (= true  (.delete d)))
+    (is (= false (.exists d)))
+    (is (= false (.isDirectory d)))
+    ; dir with a file & subdir siblings
+    (is (= true (.mkdir d)))
+    (is (= true (.createNewFile foo)))
+    (is (= true (.createNewFile bar)))
+    (is (= true (.mkdir d2)))
+    (is (= ["bar.tmp" "foo.tmp" "subdir"] (.list d)))
+    (is (= ["bar.tmp" "foo.tmp"] (.listFiles d)))
+    (is (= ["foo.tmp"] (.list d (fn [d name] (starts-with? name "foo") ) )))
+    (is (= ["bar.tmp" "subdir"] (.list d (fn [d name] (not (starts-with? name "foo")) ) )))
+    (is (= ["bar.tmp"] (.listFiles d (fn [d name] (not (starts-with? name "foo")) ) )))
+    (is (= false (.delete d)))
+    (is (= true (.exists foo)))
+    (is (every? true? (mapv #(.delete %) (list foo bar d2 d))))
+    ;derive parent dirs from subdir via mkdir
+    (is (every? false? (mapv #(.exists %) (list deep d3 d2 d))))
+    (is true (.mkdirs deep)) ; Should make b just like java, which is why core/make-parents exists
+    (is (every? true? (mapv #(and (.isDirectory %) (.exists %)) (list deep d3 d2 d))))
+    (is (every? true? (mapv #(.delete %) (list deep d3 d2 d))))))
