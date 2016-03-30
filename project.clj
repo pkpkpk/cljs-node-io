aace1663108904cb94477ee3de759463953df1d5
pat <pat.killean@gmail.com>
2016-03-29 21:04:51 -0400

    bump cljs & fw

--- project.clj
+++ project.clj
@@ -4,24 +4,22 @@
 
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/clojurescript "1.8.40"]
                  [org.clojure/core.async "0.2.374"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0-6"]]
+            [lein-figwheel "0.5.2"]]
 
 
-  :clean-targets ["server.js"
-                  "target"]
+  :clean-targets ["target"]
 
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src" "test"]
                         :figwheel true
-                        :compiler {
-                                   :parallel-build true
+                        :compiler {:parallel-build true
                                    :main cljs-node-io.core
-                                   :output-to "target/server_dev/cljs_node_io.js"
-                                   :output-dir "target/server_dev"
+                                   :output-to "target/out/cljs_node_io.js"
+                                   :output-dir "target/out"
                                    :target :nodejs
                                    :optimizations :none
                                    :source-map true}}
@@ -31,6 +29,7 @@
                          :notify-command ["node" "target/test/tests-simple.js"]
                          :compiler {:optimizations :simple
                                     :target :nodejs
+                                    :parallel-build true
                                     :main cljs-node-io.core
                                     :output-to "target/test/tests-simple.js"
                                     :output-dir "target/test/out-simple"}}]
