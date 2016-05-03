3786bebf06d82004c1ce3fc937456cc9c1aec90d
Pat Killean <pat.killean@gmail.com>
2016-05-03 16:11:02 -0400

    add try-true macro

--- src/cljs_node_io/macros.clj
+++ src/cljs_node_io/macros.clj
@@ -1,25 +1,6 @@
 (ns cljs-node-io.macros
    (:refer-clojure :exclude [with-open]))
 
-
-(defmacro with-open
-  "bindings => [name init ...]
-  Evaluates body in a try expression with names bound to the values
-  of the inits, and a finally clause that calls (.close name) on each
-  name in reverse order."
-  [bindings & body]
-  (assert (vector? bindings) "a vector for its binding")
-  (assert (even? (count bindings)) "an even number of forms in binding vector")
-  (cond
-    (= (count bindings) 0) `(do ~@body)
-    (symbol? (bindings 0)) `(let ~(subvec bindings 0 2)
-                              (try
-                                (with-open ~(subvec bindings 2) ~@body)
-                                (finally
-                                  (. ~(bindings 0) close))))
-    :else  (throw (IllegalArgumentException. "with-open only allows Symbols in bindings"))))
-
-
 ;https://github.com/markmandel/while-let
 (defmacro while-let
   "Repeatedly executes body while test expression is true, evaluating the body with binding-form bound to the value of test."
@@ -28,3 +9,11 @@
        (when ~form
            ~@body
            (recur ~test))))
+
+(defmacro try-true
+  [expr]
+  `(try
+     (do
+       ~expr
+       true)
+     (catch ~'js/Error ~'e false)))
