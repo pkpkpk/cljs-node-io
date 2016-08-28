9ab5e3d3a682f1ab4053d04f7fd1ccc7197c07f5
Patrick Killean <pat.killean@gmail.com>
2016-08-27 20:12:12 -0400

    bump cljs v

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [com.cognitect/transit-clj "0.8.285"]
-                 [org.clojure/clojurescript "1.9.225"]
+                 [org.clojure/clojurescript "1.9.227"]
                  [org.clojure/test.check "0.9.0" :scope "test"]
                  [andare "0.1.0"]]
 
