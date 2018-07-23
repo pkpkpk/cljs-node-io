422617969a537ae45635fa3d45d4beb1de93d3eb
Patrick Killean <pat.killean@gmail.com>
2018-07-23 06:58:03 -0400

    simplify runner

--- src/test/cljs_node_io/runner.cljs
+++ src/test/cljs_node_io/runner.cljs
@@ -18,17 +18,17 @@
     (if (and (string/starts-with? a "{") (string/ends-with? a "}"))
        (read-string a))))
 
-(defn -main [& args] ; uid+gid, tmpdir, select files & tests
-  (let [opts (get-opts args) ; {:port 1234  :localAddress "..." :host "...."}
-        client (if opts (wire/start-client (clj->js opts)))]
-    (if-not (fexists? "src/test/cljs_node_io/fork_test.js")
-      (throw (js/Error. "tests must be run from project root!"))
-      (wire/run-tests
-        'cljs-node-io.core-tests
-        'cljs-node-io.fs-tests
-        'cljs-node-io.file-tests
-        'cljs-node-io.spawn-tests
-        'cljs-node-io.async-tests))))
+(defn -main [& args]
+  ; {:port 1234  :localAddress "..." :host "...."}
+  (some-> (get-opts args) clj->js wire/start-client)
+  (if-not (fexists? "src/test/cljs_node_io/fork_test.js")
+    (throw (js/Error. "tests must be run from project root!"))
+    (wire/run-tests
+     'cljs-node-io.core-tests
+     'cljs-node-io.fs-tests
+     'cljs-node-io.file-tests
+     'cljs-node-io.spawn-tests
+     'cljs-node-io.async-tests)))
 
 (set! *main-cli-fn* -main)
 
