ef86711af6474ebbc4f85539131630267cf205bd
Patrick Killean <pat.killean@gmail.com>
2018-07-24 02:18:12 -0400

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-node-io "1.0.0"
+(defproject cljs-node-io "1.1.0-SNAPSHOT"
   :description "A ClojureScript IO Library for NodeJS"
   :url "https://github.com/pkpkpk/cljs-node-io"
   :license {:name "Eclipse Public License"
